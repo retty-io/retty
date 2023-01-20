a66c53321ccdbfa84c16ec568d8af03d2c31dd52
yngrtc <y@ngr.tc>
2023-01-19 23:52:10 -0800

    fix bootstrap_tcp_*

--- src/bootstrap/mod.rs
+++ src/bootstrap/mod.rs
@@ -4,10 +4,10 @@ use crate::transport::AsyncTransportWrite;
 use std::future::Future;
 use std::pin::Pin;
 
-/*pub mod bootstrap_tcp_client;
+pub mod bootstrap_tcp_client;
 pub mod bootstrap_tcp_server;
-pub mod bootstrap_udp_client;
-pub mod bootstrap_udp_server;*/
+//pub mod bootstrap_udp_client;
+//pub mod bootstrap_udp_server;
 
 pub type PipelineFactoryFn = Box<
     dyn (Fn(
