c0731e0ad389f53e2ae346cacff550c0249ffd94
yngrtc <y@ngr.tc>
2023-02-05 15:26:32 -0800

    add sans-io udp server/client examples

---
+++ examples/sans-io/sans_io_udp_client.rs
@@ -0,0 +1,176 @@
+use clap::Parser;
+use std::io::stdin;
+use std::io::Write;
+use std::net::SocketAddr;
+use std::str::FromStr;
+use std::sync::Arc;
+use tokio::sync::broadcast::Sender;
+
+use retty::bootstrap::BootstrapUdpClient;
+use retty::channel::{
+    Handler, InboundContext, InboundHandler, OutboundContext, OutboundHandler, Pipeline,
+};
+use retty::codec::{
+    byte_to_message_decoder::{LineBasedFrameDecoder, TaggedByteToMessageCodec, TerminatorType},
+    string_codec::{TaggedString, TaggedStringCodec},
+};
+use retty::runtime::default_runtime;
+use retty::transport::{AsyncTransport, TaggedBytesMut, TransportContext};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+struct TaggedSansIODecoder;
+struct TaggedSansIOEncoder;
+struct TaggedSansIOHandler {
+    decoder: TaggedSansIODecoder,
+    encoder: TaggedSansIOEncoder,
+}
+
+impl TaggedSansIOHandler {
+    fn new() -> Self {
+        TaggedSansIOHandler {
+            decoder: TaggedSansIODecoder,
+            encoder: TaggedSansIOEncoder,
+        }
+    }
+}
+impl InboundHandler for TaggedSansIODecoder {
+    type Rin = TaggedString;
+    type Rout = Self::Rin;
+
+    fn read(&mut self, _ctx: &InboundContext<Self::Rin, Self::Rout>, msg: Self::Rin) {
+        println!(
+            "received back: {} from {:?}",
+            msg.message, msg.transport.peer_addr
+        );
+    }
+}
+
+impl OutboundHandler for TaggedSansIOEncoder {
+    type Win = TaggedString;
+    type Wout = Self::Win;
+
+    fn write(&mut self, ctx: &OutboundContext<Self::Win, Self::Wout>, msg: Self::Win) {
+        ctx.fire_write(msg);
+    }
+}
+
+impl Handler for TaggedSansIOHandler {
+    type Rin = TaggedString;
+    type Rout = Self::Rin;
+    type Win = TaggedString;
+    type Wout = Self::Win;
+
+    fn name(&self) -> &str {
+        "TaggedSansIOHandler"
+    }
+
+    fn split(
+        self,
+    ) -> (
+        Box<dyn InboundHandler<Rin = Self::Rin, Rout = Self::Rout>>,
+        Box<dyn OutboundHandler<Win = Self::Win, Wout = Self::Wout>>,
+    ) {
+        (Box::new(self.decoder), Box::new(self.encoder))
+    }
+}
+
+#[derive(Parser)]
+#[command(name = "SansIO UDP Client")]
+#[command(author = "Rusty Rain <y@liu.mx>")]
+#[command(version = "0.1.0")]
+#[command(about = "An example of sans-io udp client", long_about = None)]
+struct Cli {
+    #[arg(short, long)]
+    debug: bool,
+    #[arg(long, default_value_t = format!("0.0.0.0"))]
+    host: String,
+    #[arg(long, default_value_t = 8080)]
+    port: u16,
+    #[arg(long, default_value_t = format!("INFO"))]
+    log_level: String,
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let cli = Cli::parse();
+    let host = cli.host;
+    let port = cli.port;
+    let log_level = log::LevelFilter::from_str(&cli.log_level)?;
+    if cli.debug {
+        env_logger::Builder::new()
+            .format(|buf, record| {
+                writeln!(
+                    buf,
+                    "{}:{} [{}] {} - {}",
+                    record.file().unwrap_or("unknown"),
+                    record.line().unwrap_or(0),
+                    record.level(),
+                    chrono::Local::now().format("%H:%M:%S.%6f"),
+                    record.args()
+                )
+            })
+            .filter(None, log_level)
+            .init();
+    }
+
+    println!("Connecting {}:{}...", host, port);
+
+    let transport = TransportContext {
+        local_addr: SocketAddr::from_str("0.0.0.0:0")?,
+        peer_addr: Some(SocketAddr::from_str(&format!("{}:{}", host, port))?),
+    };
+
+    let mut bootstrap = BootstrapUdpClient::new(default_runtime().unwrap());
+    bootstrap.pipeline(Box::new(move |writer: Sender<TaggedBytesMut>| {
+        Box::pin(async move {
+            let pipeline: Pipeline<TaggedBytesMut, TaggedString> = Pipeline::new();
+
+            let async_transport_handler = AsyncTransport::new(writer);
+            let line_based_frame_decoder_handler = TaggedByteToMessageCodec::new(Box::new(
+                LineBasedFrameDecoder::new(8192, true, TerminatorType::BOTH),
+            ));
+            let string_codec_handler = TaggedStringCodec::new();
+            let sans_io_handler = TaggedSansIOHandler::new();
+
+            pipeline.add_back(async_transport_handler).await;
+            pipeline.add_back(line_based_frame_decoder_handler).await;
+            pipeline.add_back(string_codec_handler).await;
+            pipeline.add_back(sans_io_handler).await;
+            pipeline.finalize().await;
+
+            Arc::new(pipeline)
+        })
+    }));
+
+    bootstrap.bind(transport.local_addr).await?;
+
+    let pipeline = bootstrap
+        .connect(transport.peer_addr.as_ref().unwrap())
+        .await?;
+
+    println!("Enter bye to stop");
+    let mut buffer = String::new();
+    while stdin().read_line(&mut buffer).is_ok() {
+        match buffer.trim_end() {
+            "" => break,
+            line => {
+                pipeline
+                    .write(TaggedString {
+                        transport,
+                        message: format!("{}\r\n", line),
+                    })
+                    .await;
+                if line == "bye" {
+                    pipeline.close().await;
+                    break;
+                }
+            }
+        };
+        buffer.clear();
+    }
+
+    bootstrap.stop().await;
+
+    Ok(())
+}
---
+++ examples/sans-io/sans_io_udp_server.rs
@@ -0,0 +1,190 @@
+use clap::Parser;
+use std::io::Write;
+use std::str::FromStr;
+use std::sync::Arc;
+use std::time::{Duration, Instant};
+use tokio::sync::broadcast::Sender;
+
+use retty::bootstrap::BootstrapUdpServer;
+use retty::channel::{
+    Handler, InboundContext, InboundHandler, OutboundContext, OutboundHandler, Pipeline,
+};
+use retty::codec::{
+    byte_to_message_decoder::{LineBasedFrameDecoder, TaggedByteToMessageCodec, TerminatorType},
+    string_codec::{TaggedString, TaggedStringCodec},
+};
+use retty::runtime::default_runtime;
+use retty::transport::{AsyncTransport, TaggedBytesMut, TransportContext};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+struct TaggedSansIODecoder {
+    interval: Duration,
+    timeout: Instant,
+    last_transport: Option<TransportContext>,
+}
+struct TaggedSansIOEncoder;
+struct TaggedSansIOHandler {
+    decoder: TaggedSansIODecoder,
+    encoder: TaggedSansIOEncoder,
+}
+
+impl TaggedSansIOHandler {
+    fn new(interval: Duration) -> Self {
+        TaggedSansIOHandler {
+            decoder: TaggedSansIODecoder {
+                timeout: Instant::now() + interval,
+                interval,
+                last_transport: None,
+            },
+            encoder: TaggedSansIOEncoder,
+        }
+    }
+}
+
+impl InboundHandler for TaggedSansIODecoder {
+    type Rin = TaggedString;
+    type Rout = Self::Rin;
+
+    fn read(&mut self, ctx: &InboundContext<Self::Rin, Self::Rout>, msg: Self::Rin) {
+        println!(
+            "handling {} from {:?}",
+            msg.message, msg.transport.peer_addr
+        );
+        if msg.message == "bye" {
+            self.last_transport.take();
+        } else {
+            self.last_transport = Some(msg.transport);
+            ctx.fire_write(TaggedString {
+                transport: msg.transport,
+                message: format!("{}\r\n", msg.message),
+            });
+        }
+    }
+
+    fn read_timeout(&mut self, ctx: &InboundContext<Self::Rin, Self::Rout>, now: Instant) {
+        if self.last_transport.is_some() && self.timeout <= now {
+            println!("TaggedSansIOHandler timeout at: {:?}", self.timeout);
+            self.timeout = now + self.interval;
+            if let Some(transport) = &self.last_transport {
+                ctx.fire_write(TaggedString {
+                    transport: *transport,
+                    message: format!("Keep-alive message: next one at {:?}\r\n", self.timeout),
+                });
+            }
+        }
+
+        //last handler, no need to fire_read_timeout
+    }
+    fn poll_timeout(&mut self, _ctx: &InboundContext<Self::Rin, Self::Rout>, eto: &mut Instant) {
+        if self.last_transport.is_some() && self.timeout < *eto {
+            *eto = self.timeout;
+        }
+
+        //last handler, no need to fire_poll_timeout
+    }
+}
+
+impl OutboundHandler for TaggedSansIOEncoder {
+    type Win = TaggedString;
+    type Wout = Self::Win;
+
+    fn write(&mut self, ctx: &OutboundContext<Self::Win, Self::Wout>, msg: Self::Win) {
+        ctx.fire_write(msg);
+    }
+}
+
+impl Handler for TaggedSansIOHandler {
+    type Rin = TaggedString;
+    type Rout = Self::Rin;
+    type Win = TaggedString;
+    type Wout = Self::Win;
+
+    fn name(&self) -> &str {
+        "TaggedSansIOHandler"
+    }
+
+    fn split(
+        self,
+    ) -> (
+        Box<dyn InboundHandler<Rin = Self::Rin, Rout = Self::Rout>>,
+        Box<dyn OutboundHandler<Win = Self::Win, Wout = Self::Wout>>,
+    ) {
+        (Box::new(self.decoder), Box::new(self.encoder))
+    }
+}
+
+#[derive(Parser)]
+#[command(name = "SansIO UDP Server")]
+#[command(author = "Rusty Rain <y@liu.mx>")]
+#[command(version = "0.1.0")]
+#[command(about = "An example of sans-io udp server", long_about = None)]
+struct Cli {
+    #[arg(short, long)]
+    debug: bool,
+    #[arg(long, default_value_t = format!("0.0.0.0"))]
+    host: String,
+    #[arg(long, default_value_t = 8080)]
+    port: u16,
+    #[arg(long, default_value_t = format!("INFO"))]
+    log_level: String,
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let cli = Cli::parse();
+    let host = cli.host;
+    let port = cli.port;
+    let log_level = log::LevelFilter::from_str(&cli.log_level)?;
+    if cli.debug {
+        env_logger::Builder::new()
+            .format(|buf, record| {
+                writeln!(
+                    buf,
+                    "{}:{} [{}] {} - {}",
+                    record.file().unwrap_or("unknown"),
+                    record.line().unwrap_or(0),
+                    record.level(),
+                    chrono::Local::now().format("%H:%M:%S.%6f"),
+                    record.args()
+                )
+            })
+            .filter(None, log_level)
+            .init();
+    }
+
+    println!("listening {}:{}...", host, port);
+
+    let mut bootstrap = BootstrapUdpServer::new(default_runtime().unwrap());
+    bootstrap.pipeline(Box::new(move |writer: Sender<TaggedBytesMut>| {
+        Box::pin(async move {
+            let pipeline: Pipeline<TaggedBytesMut, TaggedString> = Pipeline::new();
+
+            let async_transport_handler = AsyncTransport::new(writer);
+            let line_based_frame_decoder_handler = TaggedByteToMessageCodec::new(Box::new(
+                LineBasedFrameDecoder::new(8192, true, TerminatorType::BOTH),
+            ));
+            let string_codec_handler = TaggedStringCodec::new();
+            let sans_io_handler = TaggedSansIOHandler::new(Duration::from_secs(5));
+
+            pipeline.add_back(async_transport_handler).await;
+            pipeline.add_back(line_based_frame_decoder_handler).await;
+            pipeline.add_back(string_codec_handler).await;
+            pipeline.add_back(sans_io_handler).await;
+            pipeline.finalize().await;
+
+            Arc::new(pipeline)
+        })
+    }));
+
+    bootstrap.bind(format!("{}:{}", host, port)).await?;
+
+    println!("Press ctrl-c to stop");
+    tokio::select! {
+        _ = tokio::signal::ctrl_c() => {
+            bootstrap.stop().await;
+        }
+    };
+
+    Ok(())
+}
