79db95cb4398b3039d539f64c22e16f4640522c1
yngrtc <y@ngr.tc>
2023-03-10 18:03:12 -0800

    remove Send + Sync from PipelineFactoryFn

--- src/bootstrap/mod.rs
+++ src/bootstrap/mod.rs
@@ -15,6 +15,6 @@ pub use bootstrap_server_tcp::BootstrapServerTcp;
 pub use bootstrap_server_udp::BootstrapServerUdp;
 
 /// Creates a new [Pipeline]
-pub type PipelineFactoryFn<R, W> = Box<dyn (Fn(Tx<R>) -> Rc<Pipeline<R, W>>) + Send + Sync>;
+pub type PipelineFactoryFn<R, W> = Box<dyn (Fn(Tx<R>) -> Rc<Pipeline<R, W>>)>;
 
 const MAX_DURATION_IN_SECS: u64 = 86400; // 1 day
